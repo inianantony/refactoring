3557e576896773fd498d05e21e9127ee50d9bca4
Inian Antony <inianantony.s@gmail.com>
2020-01-05 20:50:06 +0800

    add unit test project

---
+++ trivia/C#/Trivia/Trivia.UnitTests/GameRunnerTests.cs
@@ -0,0 +1,14 @@
+﻿using NUnit.Framework;
+
+namespace Trivia.UnitTests
+{
+    [TestFixture]
+    public class GameRunnerTests
+    {
+        [Test]
+        public void Main_ShouldNotThrowAnyException()
+        {
+            Assert.DoesNotThrow(() => GameRunner.Main(null));
+        }
+    }
+}
