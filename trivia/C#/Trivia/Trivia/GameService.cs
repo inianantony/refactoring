6bfeeb64f61fed2487df460236bebfd3d317b7b7
Inian Antony <inianantony.s@gmail.com>
2020-01-05 20:57:20 +0800

    Extracted from main method to game service

---
+++ trivia/C#/Trivia/Trivia/GameService.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using UglyTrivia;
+
+namespace Trivia
+{
+    public class GameService
+    {
+        private static bool _notAWinner;
+
+        public static void BeginGame()
+        {
+            Game aGame = new Game();
+
+            aGame.add("Chet");
+            aGame.add("Pat");
+            aGame.add("Sue");
+
+            Random rand = new Random();
+
+            do
+            {
+                aGame.roll(rand.Next(5) + 1);
+
+                if (rand.Next(9) == 7)
+                {
+                    _notAWinner = aGame.wrongAnswer();
+                }
+                else
+                {
+                    _notAWinner = aGame.wasCorrectlyAnswered();
+                }
+            } while (_notAWinner);
+        }
+    }
+}
