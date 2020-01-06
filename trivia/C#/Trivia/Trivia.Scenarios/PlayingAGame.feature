37cb4ce308d911658b22c79eaed7654b7ec16cfa
Inian Antony <inianantony.s@gmail.com>
2020-01-06 15:26:05 +0800

    add feature file for covering scenario

---
+++ trivia/C#/Trivia/Trivia.Scenarios/PlayingAGame.feature
@@ -0,0 +1,10 @@
+﻿Feature: PlayingAGame
+	Simulating the playing of game by 1 player or multiplayer
+
+@mytag
+Scenario: There is only 1 player in the game and he is answering corret for 6 times
+	Given Player "Chet" have joined the game
+	And Rolled 2 during the game play
+	When Player answers the question correctly
+	And Repeat rolling 2 and answering correctly for 5 more times
+	Then The last correct answer should return false
---
+++ trivia/C#/Trivia/Trivia.Scenarios/Steps/StepDefinition.cs
@@ -0,0 +1,51 @@
+﻿using NUnit.Framework;
+using TechTalk.SpecFlow;
+using UglyTrivia;
+
+namespace Trivia.Scenarios.Steps
+{
+    [Binding]
+    public class StepDefinition
+    {
+        private readonly Game _game = new Game();
+
+        private bool _result = true;
+
+        [Given(@"Player ""(.*)"" have joined the game")]
+        public void GivenPlayerHaveJoinedTheGame(string playerName)
+        {
+            _game.Add(playerName);
+        }
+
+        [Given(@"Rolled (.*) during the game play")]
+        public void GivenRolledDuringTheGamePlay(int roll)
+        {
+            _game.Roll(roll);
+        }
+
+        [When(@"Player answers the question correctly")]
+        public void WhenPlayerAnswersTheQuestionCorrectly()
+        {
+            _result = _game.WasCorrectlyAnswered();
+        }
+
+        [When(@"Repeat rolling (.*) and answering correctly for (.*) more times")]
+        public void WhenRepeatRollingAndAnsweringCorrectlyForMoreTimes(int roll, int times)
+        {
+            for (int i = 1; i <= times; i++)
+            {
+                GivenRolledDuringTheGamePlay(roll);
+                WhenPlayerAnswersTheQuestionCorrectly();
+            }
+        }
+
+
+        [Then(@"The last correct answer should return false")]
+        public void ThenTheLastCorrectAnswerShouldReturnFalse()
+        {
+            Assert.AreEqual(false, _result);
+        }
+
+
+    }
+}
