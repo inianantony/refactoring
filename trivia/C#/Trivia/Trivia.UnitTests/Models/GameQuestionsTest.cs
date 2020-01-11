beab477648d92e598374d96f25d105df45538ebd
Inian Antony <inianantony.s@gmail.com>
2020-01-11 17:20:21 +0800

    add GameQuestionsTest

---
+++ trivia/C#/Trivia/Trivia.UnitTests/Models/GameQuestionsTest.cs
@@ -0,0 +1,22 @@
+﻿using NUnit.Framework;
+using Trivia.Models;
+
+namespace Trivia.UnitTests.Models
+{
+    [TestFixture]
+    public class GameQuestionsTest
+    {
+        private GameQuestions _gameQuestions;
+        [SetUp]
+        public void Init()
+        {
+            _gameQuestions = new GameQuestions();
+        }
+
+        [TestCase(0,ExpectedResult = "Pop")]
+        public string CurrentCategory_ShouldReturn_RespectiveCategory(int place)
+        {
+            return _gameQuestions.CurrentCategory(place);
+        }
+    }
+}
