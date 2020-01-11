9e216d159095e49dbf0ec50880e96a138726c3f7
Inian Antony <inianantony.s@gmail.com>
2020-01-11 17:42:43 +0800

    add RandomizerTests

---
+++ trivia/C#/Trivia/Trivia.UnitTests/Services/RandomizerTests.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using NUnit.Framework;
+using Trivia.Services;
+
+namespace Trivia.UnitTests.Services
+{
+    [TestFixture]
+    public class RandomizerTests
+    {
+        [Test]
+        public void NextRandomNumber_ShouldReturnANumberGreaterThan_Minus_1()
+        {
+            Randomizer randomizer = new Randomizer(new Random());
+            Assert.Greater(randomizer.NextRandomNumber(9), -1);
+        }
+    }
+}
