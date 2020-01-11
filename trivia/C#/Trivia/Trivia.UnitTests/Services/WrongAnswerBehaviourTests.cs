cb5dc5429113b34f04b176baeb5a7ec039614ea8
Inian Antony <inianantony.s@gmail.com>
2020-01-11 18:02:02 +0800

    add WrongAnswerBehaviourTests

---
+++ trivia/C#/Trivia/Trivia.UnitTests/Services/WrongAnswerBehaviourTests.cs
@@ -0,0 +1,25 @@
+﻿using Moq;
+using NUnit.Framework;
+using Trivia.Models;
+using Trivia.Services;
+
+namespace Trivia.UnitTests.Services
+{
+    [TestFixture]
+    public class WrongAnswerBehaviourTests
+    {
+        [Test]
+        public void MakeWrongAnswer_ShouldCall_GivePenaltyToCurrentPlayer_FromGamePlayers()
+        {
+            var gamePlayers = new GamePlayers();
+            gamePlayers.AddPlayer(new Player("A"));
+            var wrongAnswerBehaviour = new WrongAnswerBehaviour(gamePlayers);
+
+            Assert.AreEqual(false, gamePlayers.CurrentPlayerIsInPenalty);
+
+            wrongAnswerBehaviour.MakeWrongAnswer();
+
+            Assert.AreEqual(true, gamePlayers.CurrentPlayerIsInPenalty);
+        }
+    }
+}
