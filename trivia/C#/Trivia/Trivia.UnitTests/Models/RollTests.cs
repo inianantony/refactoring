ea7df53bb64921827ab5c2a01daa70ff37e8502e
Inian Antony <inianantony.s@gmail.com>
2020-01-11 17:38:09 +0800

    add RollTests

---
+++ trivia/C#/Trivia/Trivia.UnitTests/Models/RollTests.cs
@@ -0,0 +1,32 @@
+﻿using NUnit.Framework;
+using Trivia.Models;
+
+namespace Trivia.UnitTests.Models
+{
+    [TestFixture]
+    public class RollTests
+    {
+        private Roll _roll;
+
+        [Test]
+        public void Value_ShouldReturn_2_WhenRollSentIs_0()
+        {
+            _roll = new Roll(2);
+            Assert.AreEqual(2, _roll.Value);
+        }
+
+        [Test]
+        public void IsOddRoll_ShouldReturn_True_WhenRollvalueIsOdd()
+        {
+            _roll = new Roll(3);
+            Assert.AreEqual(true, _roll.IsOddRoll());
+        }
+
+        [Test]
+        public void IsOddRoll_ShouldReturn_False_WhenRollvalueIsEven()
+        {
+            _roll = new Roll(2);
+            Assert.AreEqual(false, _roll.IsOddRoll());
+        }
+    }
+}
