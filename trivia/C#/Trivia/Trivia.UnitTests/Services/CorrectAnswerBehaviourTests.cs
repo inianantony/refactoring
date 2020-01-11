508839a5f8fa0a53c2935f416eec38bf92eac8fb
Inian Antony <inianantony.s@gmail.com>
2020-01-11 18:38:58 +0800

    add CorrectAnswerBehaviourTests

---
+++ trivia/C#/Trivia/Trivia.UnitTests/Services/CorrectAnswerBehaviourTests.cs
@@ -0,0 +1,22 @@
+﻿using NUnit.Framework;
+using Trivia.Models;
+using Trivia.Services;
+
+namespace Trivia.UnitTests.Services
+{
+    [TestFixture]
+    public class CorrectAnswerBehaviourTests
+    {
+        [Test]
+        public void MakeCorrectAnswer_ShouldAddOnePointToPlayer()
+        {
+            var gamePlayers = new GamePlayers();
+            gamePlayers.AddPlayer(new Player("A"));
+            CorrectAnswerBehaviour correctAnswerBehaviour = new CorrectAnswerBehaviour(gamePlayers);
+
+            correctAnswerBehaviour.MakeCorrectAnswer();
+
+            Assert.AreEqual(1, gamePlayers.CurrentPlayerPoints);
+        }
+    }
+}
