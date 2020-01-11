864f19f7510eead6959d045760602296a669b93d
Inian Antony <inianantony.s@gmail.com>
2020-01-11 18:43:40 +0800

    add case for CorrectAnswerBehaviourTests

--- trivia/C#/Trivia/Trivia.UnitTests/Services/CorrectAnswerBehaviourTests.cs
+++ trivia/C#/Trivia/Trivia.UnitTests/Services/CorrectAnswerBehaviourTests.cs
@@ -8,15 +8,32 @@ namespace Trivia.UnitTests.Services
     public class CorrectAnswerBehaviourTests
     {
         [Test]
-        public void MakeCorrectAnswer_ShouldAddOnePointToPlayer()
+        public void MakeCorrectAnswer_ShouldAddOnePointToPlayer_IfPlayerDidntHavePenalty()
         {
             var gamePlayers = new GamePlayers();
-            gamePlayers.AddPlayer(new Player("A"));
+            var player = new Player("A");
+            gamePlayers.AddPlayer(player);
             CorrectAnswerBehaviour correctAnswerBehaviour = new CorrectAnswerBehaviour(gamePlayers);
 
             correctAnswerBehaviour.MakeCorrectAnswer();
 
+            Assert.AreEqual(false, player.Penalty);
             Assert.AreEqual(1, gamePlayers.CurrentPlayerPoints);
+        }
+
+        [Test]
+        public void MakeCorrectAnswer_Should_Not_AddPointToPlayer_IfPlayerHavePenalty()
+        {
+            var gamePlayers = new GamePlayers();
+            var player = new Player("A");
+            gamePlayers.AddPlayer(player);
+            CorrectAnswerBehaviour correctAnswerBehaviour = new CorrectAnswerBehaviour(gamePlayers);
+            player.SetPenalty();
+
+            correctAnswerBehaviour.MakeCorrectAnswer();
+
+            Assert.AreEqual(true, player.Penalty);
+            Assert.AreEqual(0, gamePlayers.CurrentPlayerPoints);
         }
     }
 }
